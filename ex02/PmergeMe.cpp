3ddf51e3e6b71071b5396f6b59d7e9307835f393
edsardgrisel <edsardgrisel1@gmail.com>
2026-06-05 17:13:17 +0200

    should be done. still need to test and implement for vector as well

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,6 +1,7 @@
 
 #include "PmergeMe.hpp"
 
+#include <algorithm>
 #include <chrono>
 #include <cmath>
 #include <deque>
@@ -100,37 +101,50 @@ void PmergeMe::createPairs(std::deque<int>& container, std::deque<int>& winners,
     losers = std::move(newLosers);
 }
 
-
-void /*PmergeMe::*/insertRange(size_t firstToInsert, size_t lastToInsert, std::deque<int>& winners, std::deque<int>& losers, const std::deque<int> origWinners)
+void /*PmergeMe::*/ insertRange(size_t firstToInsert, size_t lastToInsert, std::deque<int>& winners,
+                                std::deque<int>& sortedLosers, const std::deque<int> origWinners)
 {
-    if (firstToInsert > losers.size() - 1)
-        firstToInsert = losers.size() - 1;
-    
+    if (sortedLosers.empty())
+        return;
+
+    if (firstToInsert >= sortedLosers.size())
+        firstToInsert = sortedLosers.size() - 1;
+
     if (lastToInsert > firstToInsert)
         return;
 
-    for (size_t i = firstToInsert; i >= lastToInsert; i--)
+    for (int li = static_cast<int>(firstToInsert); li >= static_cast<int>(lastToInsert); --li)
     {
-        size_t winnerIndex = 0;
-        for (; winnerIndex < winners.size(); winnerIndex++)
+        if (static_cast<size_t>(li) < origWinners.size())
         {
-            if (winners[winnerIndex] == origWinners[i])
-                break;
-        }
-        std::deque<int>::iterator currentWinnerIt = winners.begin() + winnerIndex;
-        auto insertPos = std::lower_bound(winners.begin(), currentWinnerIt, losers[i]);
-        winners.insert(insertPos, losers[i]);
+            int winnerValue = origWinners[li];
+            std::deque<int>::iterator currentWinnerIt = std::find(winners.begin(), winners.end(), winnerValue);
 
+            if (currentWinnerIt == winners.end())
+                continue;
+
+            std::deque<int>::iterator insertPos = std::lower_bound(winners.begin(), currentWinnerIt, sortedLosers[li]);
+            winners.insert(insertPos, sortedLosers[li]);
+        }
+        else
+        {
+            // insert the unpaired loser if present
+            auto insertPos = std::lower_bound(winners.begin(), winners.end(), sortedLosers[li]);
+            winners.insert(insertPos, sortedLosers[li]);
+        }
     }
-    
-    
-    
 }
 
 void PmergeMe::sort(std::deque<int>& winners, std::deque<int>& losers)
 {
-    if (winners.size() == 1)
+    if (winners.size() <= 1)
+    {
+        std::deque<int> origWinners = winners;
+        if (!losers.empty())
+            insertRange(losers.size() - 1, 0, winners, losers, origWinners);
+        losers.clear();
         return;
+    }
 
     std::deque<int> newWinners;
     std::deque<int> newLosers;
@@ -151,26 +165,36 @@ void PmergeMe::sort(std::deque<int>& winners, std::deque<int>& losers)
     if (losers.size() != winners.size())
         sortedLosers.push_back(losers.back());
 
-    const std::deque<int> constWinners = winners;
-
+    const std::deque<int> origWinners = newWinners;
 
     int previousJacobsthal = -1;
-    for (int index = 0; ; ++index)
+    for (int index = 0;; ++index)
     {
         int jacobsthal = generateJacobsthal(index);
 
         if (jacobsthal == previousJacobsthal)
             continue;
 
-
-        insertRange(jacobsthal, generateJacobsthal(index - 1) + 1, winners, sortedLosers, constWinners);
-
+        // insert remaining losers beyond the last jacobsthal number that fits in the list length
         if (jacobsthal >= static_cast<int>(sortedLosers.size()))
+        {
+            int last = static_cast<int>(sortedLosers.size()) - 1;
+            if (last >= previousJacobsthal + 1)
+                insertRange(static_cast<size_t>(last), static_cast<size_t>(previousJacobsthal + 1),
+                            newWinners, sortedLosers, origWinners);
             break;
+        }
+
+        // insert range [previousJacobsthal+1 .. jacobsthal] (right-to-left inside)
+        if (jacobsthal >= previousJacobsthal + 1)
+            insertRange(static_cast<size_t>(jacobsthal),
+                        static_cast<size_t>(previousJacobsthal + 1), newWinners, sortedLosers,
+                        origWinners);
 
         previousJacobsthal = jacobsthal;
-
     }
+
+    winners = std::move(newWinners);
 }
 
 float PmergeMe::run(std::deque<int>& container)
@@ -184,6 +208,8 @@ float PmergeMe::run(std::deque<int>& container)
 
     createPairs(container, winners, losers);
     sort(winners, losers);
+
+    container = winners;
 
     std::chrono::_V2::system_clock::time_point end = std::chrono::high_resolution_clock::now();
     std::chrono::microseconds::rep             timeDifference =
